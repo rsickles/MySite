1ece2033ba3887b2f8ee2743e27e7b6bec3e0e50
rsickles9 <rsickles9@gmail.com>
2020-05-13 22:35:38 -0400

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,14 @@ import Col from 'react-bootstrap/Col';
 function Footer(){
 
 	return (
-		<footer className="mt-5">
+		<footer className="mt-2">
             <Container fluid={true}>
                 <Row className="border-top justify-content-between p-3">
                     <Col className="p-0" md={3} sm={12}>
                         Email: rsickles9@gmail.com
                     </Col>
-                    <Col className="p-0 d-flex justify-content-end" md={3}>
-                        This site was made by Ryan Sickles.
+                    <Col className="p-0 d-flex justify-content-end" md={6} sm={12}>
+                        This site was made with&nbsp;<a href='https://reactjs.org/'>React.JS</a>&nbsp;by Ryan Sickles.
                     </Col>
                 </Row>
             </Container>
